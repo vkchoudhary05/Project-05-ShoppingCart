ee8b7bd0c53cf5aa12a80ed02593ceb7ba0525b1
vkchoudhary05 <102945131+vkchoudhary05@users.noreply.github.com>
2022-05-25 12:00:01 +0530

    Modules

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-Parser');
+const mongoose = require('mongoose');
+const multer = require('multer')
+
+const route = require('./routers/routes.js')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use( multer().any())
+
+mongoose.connect("mongodb+srv://new-user_2020:Vivek2020@cluster0.lxwkw.mongodb.net/Project05", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function (){
+    console.log('Express app running on port: ' + (process.env.PORT || 3000))
+});
+
+
